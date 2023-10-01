d5763d1792996863c22582e8f940dd99aa5de734
mcascarino3 <143422219+mcascarino3@users.noreply.github.com>
2023-10-01 18:26:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function festivalAnswer(){
+  var password = document.getElementById("festivalQuestion");
+  var passwordText = password.value
+  if(passwordText=="Cannes Lions"){
+    return true;
+  }
+alert ("Try again!")
